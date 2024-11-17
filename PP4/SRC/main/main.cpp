4ca32d6d713c629fc7be486532d5907cb4c8c331
Deislher Yadir Sánchez Funez <125695457+DeislherSanchez@users.noreply.github.com>
2024-11-17 10:19:56 -0600

    Delete PP4/SRC directory

--- PP4/SRC/main/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtkmm.h>
-#include "/home/jahir/Downloads/PP4/SRC/views/gestorTareas.h"
-
-int main(int argc, char* argv[])
-{
-    auto app = Gtk::Application::create("org.gtkmm.gestortareas");
-
-    return app->make_window_and_run<VentanaPrincipal>(argc, argv);
-}
